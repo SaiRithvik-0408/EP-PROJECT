30ba55e1a2248cd31750a719a580bf61e49f35a6
SaiRithvik-0408 <karnatisairithvik@gmail.com>
2024-03-20 10:57:31 +0530

    Initial commit

---
+++ EPPROJECT/src/main/java/com/klef/ep/services/AdminServiceImpl.java
@@ -0,0 +1,32 @@
+package com.klef.ep.services;
+
+import java.util.List;
+
+import com.klef.ep.models.Admin;
+import com.klef.ep.models.User;
+
+public class AdminServiceImpl implements AdminService
+{
+
+	
+	public Admin CheckAdminLogin(String username, String password) 
+	{
+		
+		return null;
+	}
+
+	
+	public String AddUser(User user) 
+	{
+		
+		return null;
+	}
+
+	
+	public List<User> ViewAllUsers() 
+	{
+		
+		return null;
+	}
+
+}
